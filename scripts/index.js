efdf1fb098bffc6c9590ae51850249c1d8809006
Alexandre Beaujour <abeaujour@steamulo.com>
2020-06-17 18:44:29 +0200

    almost done

--- scripts/index.js
+++ scripts/index.js
@@ -22,6 +22,44 @@ function registerParentFloatDiv(element, animation) {
     }
 }
 
+function getChildrenDiv(parentEl, maxArrayWidth, windowWidth) {
+    const result = [];
+    $(parentEl).find('> *').each((indexChild, child) => {
+        result.push({
+            target: child,
+            columnWidth: Math.round(maxArrayWidth * ($(child).outerWidth() / windowWidth)),
+            height: $(child).outerHeight(),
+        })
+    });
+    return result;
+}
+
+function addLine(positions, line, maxArrayWidth) {
+    for (let column = 0; column < maxArrayWidth; column++) {
+        const array = [];
+        for (let i = 0; i < maxArrayWidth; i++) {
+            array[i] = null;
+        }
+        positions[line] = array;
+    }
+}
+
+function getNullColumnAvailable(positions, maxArrayWidth, line) {
+    const nullColumnAvailable = [];
+    let objNull = {index: null, length: null};
+    for (let i = 0; i < maxArrayWidth; i++) {
+        if (positions[line][i] === null && objNull.index === null) {
+            objNull.index = i;
+        }
+        if (objNull.index !== null && (positions[line][i] !== null || i === (maxArrayWidth - 1))) {
+            objNull.length = 1 + i - objNull.index;
+            nullColumnAvailable.push(JSON.parse(JSON.stringify(objNull)));
+            objNull = {index: null, length: null};
+        }
+    }
+    return nullColumnAvailable;
+}
+
 const FloatDiv = function (selector, animation = 200, maxArrayWidth = 12) {
     // region init
     if (animation === 0) {
@@ -32,70 +70,83 @@ const FloatDiv = function (selector, animation = 200, maxArrayWidth = 12) {
     $(selector).each((i, e) => registerParentFloatDiv(e, animation));
     for (let parent of parentDivs) {
         const positions = [];
-        $(parent.target).find('> *').each((indexChild, child) => {
-            const elWidthCategory = Math.round(maxArrayWidth * ($(child).outerWidth() / windowWidth)); // [1|2|3|4|5|6|7|8|9|10|11|12] if using bootstrap
-            let line = 0;
-            let positionFound = false;
-            const heights = [];
-            for (let i = 0; i < maxArrayWidth; i++) {
-                heights[i] = 0;
-            }
-            while (positionFound === false) {
-                const availableColumns = [];
-                for (let column = 0; column < maxArrayWidth; column++) {
-                    if (typeof positions[line] === 'undefined') {
-                        const array = [];
-                        for (let i = 0; i < maxArrayWidth; i++) {
-                            array[i] = null;
-                        }
-                        positions[line] = array;
+        const heights = [];
+        for (let i = 0; i < maxArrayWidth; i++) {
+            heights[i] = 0;
+        }
+        let line = 0;
+        const childrenDiv = getChildrenDiv(parent.target, maxArrayWidth, windowWidth);
+        addLine(positions, line, maxArrayWidth);
+        const nbChildren = childrenDiv.length;
+        for (let n = 0; n < nbChildren; n++) {
+            // region get null column available
+            let nullColumnAvailable = getNullColumnAvailable(positions, maxArrayWidth, line);
+            // endregion
+            // region find child to add
+            let childIndex = -1;
+            let indexColumn = -1;
+            while (childIndex === -1) {
+                for (const objNull of nullColumnAvailable) {
+                    childIndex = childrenDiv.findIndex(x => x.columnWidth <= objNull.length);
+                    if (childIndex !== -1) {
+                        indexColumn = objNull.index;
                     }
-                    if (positions[line][column] !== null) {
-                        heights[column] += $(positions[line][column].target).outerHeight();
-                    }
-                    // il faut tester les suivantes et voir quelles tailles est la plus petite
-                    if (
-                        positions[line][column] === null
-                        && (column + elWidthCategory) <= maxArrayWidth
-                    ) {
-                        availableColumns.push({
-                            index: column,
-                            height: heights[column],
-                            realColumnsHeight: 0,
-                        });
-                    }
-                }
-                if (availableColumns.length > 0) {
-                    for (let availableColumn of availableColumns) {
-                        for (let i = availableColumn.index; i < availableColumn.index + elWidthCategory; i++) {
-                            if (heights[i] > availableColumn.realColumnsHeight) {
-                                availableColumn.realColumnsHeight = heights[i];
+                    if (childIndex !== -1) {
+                        // region check if div on right is bigger
+                        const myColumnHeight = heights[indexColumn];
+                        let nextColumnHeight = null;
+                        let upperElement = null;
+                        if (Array.isArray(positions[line - 1])) {
+                            upperElement = positions[line - 1][indexColumn];
+                            for (let i = indexColumn; i < maxArrayWidth; i++) {
+                                if (positions[line - 1][i] === null) {
+                                    break;
+                                } else if (positions[line - 1][i].target !== upperElement.target) {
+                                    nextColumnHeight = heights[i];
+                                    break;
+                                }
                             }
                         }
+                        if (nextColumnHeight !== null && nextColumnHeight > myColumnHeight && upperElement !== null) {
+                            const newChildIndex = childrenDiv.findIndex(x => x.columnWidth <= upperElement.columnWidth);
+                            if (newChildIndex !== -1) {
+                                childIndex = newChildIndex;
+                            }
+                        }
+                        // endregion
+                        break;
                     }
-                    const column = availableColumns.reduce((prev, current) => {
-                        return (prev.realColumnsHeight <= current.realColumnsHeight) ? prev : current
-                    });
-                    for (let i = 0; i < elWidthCategory; i++) {
+                }
+                if (childIndex !== -1) {
+                    const child = childrenDiv[childIndex];
+                    for (let i = 0; i < child.columnWidth; i++) {
                         if (i === 0) {
-                            positions[line][column.index + i] = {
-                                target: child,
-                                left: (column.index / maxArrayWidth) * 100,
-                                top: column.realColumnsHeight,
+                            positions[line][indexColumn] = {
+                                target: child.target,
+                                left: (indexColumn / maxArrayWidth) * 100,
+                                top: heights[indexColumn],
+                                columnWidth: child.columnWidth,
                             };
                         } else {
-                            positions[line][column.index + i] = {
-                                target: child,
+                            positions[line][indexColumn + i] = {
+                                target: child.target,
                                 left: null,
                                 top: null,
+                                columnWidth: null,
                             };
                         }
+                        heights[indexColumn + i] += child.height;
                     }
-                    positionFound = true;
+                    childrenDiv.splice(childIndex, 1);
+                } else {
+                    line++;
+                    addLine(positions, line, maxArrayWidth);
+                    nullColumnAvailable = getNullColumnAvailable(positions, maxArrayWidth, line);
                 }
-                line++;
             }
-        });
+            // endregion
+            // console.log(nullColumnAvailable);
+        }
         for (let position of positions) {
             for (let el of position) {
                 if (el !== null && el.top !== null) {
@@ -103,6 +154,7 @@ const FloatDiv = function (selector, animation = 200, maxArrayWidth = 12) {
                 }
             }
         }
+        console.log(heights);
         console.log(positions);
     }
 };
